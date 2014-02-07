0d4fa55fa7ea037b153d101a43c8083f1b612ee1
David Beckley <beckl.ds@gmail.com>
2014-02-06 22:51:47 -0800

    Obstacles, collisions, rename, audio

--- js/game.js
+++ js/game.js
@@ -7,23 +7,16 @@ function Game(id, width, height) {
     placeholder.parentNode.replaceChild(this.canvas, placeholder);
     this.ctx = this.canvas.getContext("2d");
 
-    this.state = this.stateEnum.STARTSCREEN;
-
-    this.player = {
-        acceleration: 0.15,
-        velocity: -5,
-        displacement: height / 2
-    };
-
     /// Images
 
     this.startscreen = null;
     this.gameover = null;
 
-    // variable to hold the "thread"
-    this.interval = null;
+    /// Audio
 
-    this.space = false;
+    this.theme = null;
+
+    this.reset();
 
     var that = this;
 
@@ -32,7 +25,11 @@ function Game(id, width, height) {
         if (e.keyCode == 32)
         {
             e.preventDefault();
-            that.space = true;
+            if (that.space_ready)
+            {
+                that.space = true;
+                that.space_ready = false;
+            }
         }
     };
 
@@ -41,11 +38,43 @@ function Game(id, width, height) {
         if (e.keyCode == 32)
         {
             e.preventDefault();
-            that.space = false;
+            that.space_ready = true;
         }
     };
 
     this.load();
+}
+
+Game.prototype.reset = function()
+{
+    this.lastLoop = new Date;
+
+    this.state = this.stateEnum.STARTSCREEN;
+
+    this.player = {
+        acceleration: 0.0019,
+        velocity: 0,
+        width: 40,
+        height: 28,
+        rotation: 0,
+        x: 0,
+        y: 0,
+        score: 0
+    };
+
+    this.background_velocity = -0.23;
+
+    this.obstacle_width = 50;
+    this.obstacle_gap = 170;
+    this.obstacle_x_gap = 250;
+
+    this.obstacles = [];
+
+    this.player.y = this.height / 2 - this.player.height / 2;
+    this.player.x = this.width / 2 - this.player.width / 2;
+
+    this.space = false;
+    this.space_ready = true;
 }
 
 Game.prototype.stateEnum = {
@@ -60,12 +89,29 @@ Game.prototype.loadImage = function(filename) {
     return img;
 }
 
+Game.prototype.loadAudio = function(filename) {
+    var audio = new Audio(filename);
+    audio.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+    return audio;
+}
+
 Game.prototype.load = function() {
     this.startscreen = this.loadImage("images/splash.png");
     this.gameover = this.loadImage("images/gameover.png");
+    this.theme = this.loadAudio("audio/theme.mp3");
 }
 
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
 Game.prototype.update = function() {
+    var thisLoop = new Date;
+    var elapsed = Math.floor(thisLoop - this.lastLoop);
+    this.lastLoop = thisLoop;
+    //document.getElementById("fps").innerHTML = "Elapsed: " + elapsed;
+
     switch (this.state)
     {
         case this.stateEnum.STARTSCREEN:
@@ -74,41 +120,172 @@ Game.prototype.update = function() {
             break;
         case this.stateEnum.PLAY:
             if (this.space)
-                this.player.velocity = -4;
+            {
+                this.player.velocity = -0.55;
+                this.space = false;
+            }
 
-            this.player.velocity += this.player.acceleration;
-            this.player.displacement += this.player.velocity;
+            this.player.velocity += this.player.acceleration * elapsed;
+            this.player.y += this.player.velocity * elapsed;
+
+            if (this.player.y > this.height + this.player.width || this.player.y < -this.player.width)
+                this.state = this.stateEnum.OVER;
+            this.player.rotation = ((this.player.velocity) + this.player.rotation * 2) / 3;
+            if (this.player.rotation > (Math.PI / 2))
+                this.player.rotation = (Math.PI / 2);
+
+            var last_x = 0;
+
+            var bounds = boundingBox(this.player);
+            
+            for (var i = 0; i < this.obstacles.length; i++)
+            {
+                this.obstacles[i].x += this.background_velocity * elapsed;
+                if (collides(bounds, this.obstacles[i], this.obstacle_width, this.obstacle_gap))
+                {
+                    this.state = this.stateEnum.OVER;
+                }
+                if (this.obstacles[i].x + this.obstacle_width / 2 < this.player.x + this.player.width / 2 && !this.obstacles[i].scored)
+                {
+                    this.obstacles[i].scored = true;
+                    this.player.score++;
+                    document.getElementById("fps").innerHTML = "Score: " + this.player.score;
+                }
+                last_x = this.obstacles[i].x;
+                if (this.obstacles[i].x < -this.obstacle_width)
+                {
+                    this.obstacles.splice(i, 1);
+                    i--;
+                }
+            }
+
+            if (this.obstacles.length == 0 || last_x < this.width - this.obstacle_x_gap)
+            {
+                this.obstacles.push({x: this.width, y: Math.floor(Math.random()*((this.height-this.obstacle_gap)-0+1)+0), scored: false});
+            }
             break;
         case this.stateEnum.OVER:
+            if (this.space)
+            {
+                this.reset();
+                this.space = true;
+                this.state = this.stateEnum.PLAY;
+            }
             break;
     }
 
 
     this.render();
+
+    var that = this;
+    window.requestAnimationFrame(function() {
+        that.update()
+    });
+}
+
+function renderPlayer(ctx, player)
+{
+    // rotate and draw fish
+    ctx.save();
+
+    ctx.translate(player.x + player.width / 2, player.y + player.height / 2);
+    ctx.rotate(player.rotation);
+    ctx.fillRect(-player.width / 2, -player.height / 2, player.width, player.height);
+    ctx.strokeRect((-player.width / 2) + 0.5, (-player.height / 2) + 0.5, player.width, player.height);
+
+    ctx.restore();
 }
 
 Game.prototype.render = function() {
-    this.ctx.save();
-
-    this.ctx.fillStyle = "rgb(255,255,255)";
+    this.ctx.fillStyle = "rgb(10,10,60)";
     this.ctx.fillRect(0, 0, this.width, this.height);
-    this.ctx.fillStyle = "rgb(0,0,0)";
+    this.ctx.fillStyle = "rgb(50,120,110)";
 
-    this.ctx.fillRect(200, this.player.displacement, 30, 30);
+    renderPlayer(this.ctx, this.player);
 
+    this.ctx.fillStyle = "rgb(135, 115, 100)";
+
+    for (var i = 0; i < this.obstacles.length; i++)
+    {
+        this.ctx.fillRect(this.obstacles[i].x, 0, this.obstacle_width, this.obstacles[i].y);
+        this.ctx.strokeRect(this.obstacles[i].x + 0.5, 0.5, this.obstacle_width, this.obstacles[i].y);
+        this.ctx.fillRect(this.obstacles[i].x, this.height, this.obstacle_width, -(this.height - this.obstacle_gap - this.obstacles[i].y));
+        this.ctx.strokeRect(this.obstacles[i].x + 0.5, this.height + 0.5, this.obstacle_width, -(this.height - this.obstacle_gap - this.obstacles[i].y));
+    }
+
+    /* DISPLAY BOUNDING BOX
+    bound = boundingBox(this.player);
+    this.ctx.save();
+    this.ctx.strokeStyle = "rgb(255, 0, 0)";
+    this.ctx.strokeRect(bound.x, bound.y, bound.width, bound.height);
     this.ctx.restore();
+
+    this.ctx.fillStyle = "rgb(255,0,0)";
+    this.ctx.fillRect(this.player.x + this.player.width / 2 - 4, this.player.y + this.player.height / 2 - 4, 8, 8);
+    */
+
     this.ctx.strokeRect(0.5, 0.5, this.width - 1, this.height - 1);
 }
 
 Game.prototype.start = function() {
-    var that = this;
-    this.interval = setInterval(function() {
-        return that.update();
-    }, 10);
-}
+   var that = this;
+   window.requestAnimationFrame(function(){
+        that.update();
+    });
 
-Game.prototype.stop = function() {
-    clearInterval(this.interval);
+    this.theme.play();
 }
 
 Game.prototype.setSoundtrack = function(file) {}
+
+function collides(pb, obstacle, obstacle_width, obstacle_gap)
+{
+
+    if (pb.x + pb.width > obstacle.x && pb.x < obstacle.x + obstacle_width)
+    {
+        if (pb.y < obstacle.y || pb.y + pb.height > obstacle.y + obstacle_gap)
+            return true;
+    }
+    return false;
+}
+
+function boundingBox(player)
+{
+    var pv = [
+        {x: -player.width / 2, y: -player.height / 2},
+        {x: player.width / 2, y: -player.height / 2},
+        {x: player.width / 2, y: player.height / 2},
+        {x: -player.width / 2, y: player.height / 2}
+    ];
+
+    var max_x = false, max_y = false, min_x = false, min_y = false;
+
+    for (var i = 0; i < pv.length; i++)
+    {
+        pv[i] = rotate(pv[i].x, pv[i].y, player.rotation);
+        if (max_x === false || pv[i].x > max_x)
+            max_x = pv[i].x;
+
+        if (max_y === false || pv[i].y > max_y)
+            max_y = pv[i].y;
+
+        if (min_x === false || pv[i].x < min_x)
+            min_x = pv[i].x;
+
+        if (min_y === false || pv[i].y < min_y)
+            min_y = pv[i].y;
+    }
+
+    var width = max_x - min_x;
+    var height = max_y - min_y;
+
+    return {x: player.x - (width - player.width) / 2, y: player.y - (height - player.height) / 2, width: width, height: height};
+}
+
+function rotate(x, y, theta)
+{
+    return {
+        x: x * Math.cos(theta) - y * Math.sin(theta),
+        y: x * Math.sin(theta) - y * Math.cos(theta)
+    };
+}
